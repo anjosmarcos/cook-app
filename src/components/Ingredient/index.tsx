61b66a5030f3156267f75f9389032dade3f39415
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-05 14:40:20 -0300

    componetes da aplicação

--- src/components/Ingredient/index.tsx
+++ src/components/Ingredient/index.tsx
@@ -1,25 +1,26 @@
-import { Image, Pressable, PressableProps, Text } from "react-native";
-import { styles } from "./style";
+import { Text, Pressable, PressableProps, Image } from "react-native"
 
-export type IngredientProps = {
-    name: string;
-    image: string;
-    selected?: boolean;
+import { styles } from "./styles"
+
+export type IngredientsProps = {
+  name: string
+  image: string
+  selected?: boolean
 }
 
-export function Ingredient({ name, image, selected = false, ...rest }: IngredientProps & PressableProps){
-    return (
-        <Pressable 
-            style={[styles.container, selected && styles.selected]} 
-            {...rest}
-            >
-                <Image
-                    source={require('@/images/apple.png')}
-                    style={styles.image}
-                />
-            <Text style={styles.title}>
-                Maça
-            </Text>
-        </Pressable>
-    )
+export function Ingredient({
+  name,
+  image,
+  selected = false,
+  ...rest
+}: IngredientsProps & PressableProps) {
+  return (
+    <Pressable
+      style={[styles.container, selected && styles.selected]}
+      {...rest}
+    >
+      <Image source={{ uri: image }} style={styles.image} />
+      <Text style={styles.title}>{name}</Text>
+    </Pressable>
+  )
 }
--- src/components/Selected/index.tsx
+++ src/components/Selected/index.tsx
@@ -1,34 +1,37 @@
-import { Text, View } from "react-native"
+import { View, Text } from "react-native"
+import MaterialIcons from "@expo/vector-icons/MaterialIcons"
 import Animated, { BounceOutDown, SlideInDown } from "react-native-reanimated"
-import { MaterialIcons } from "@expo/vector-icons"
-import { styles } from "./styles"
+
 import { theme } from "@/theme"
+import { styles } from "./styles"
 
-type Props ={
-    quantity: number
-    onClear: () => void
-    onSearch: () => void
+import { Button } from "@/components/Button"
+
+type Props = {
+  quantity: number
+  onClear: () => void
+  onSearch: () => void
 }
-export function Selected({ quantity, onClear, onSearch }: Props){
-    return (
-        <Animated.View 
-            entering={SlideInDown.duration(500)}
-            exiting={BounceOutDown}
-            style={styles.container}
-            >
-            <View style={styles.header}>
-                <Text style={styles.label}>
-                    {quantity} ingredients selecionados
-                </Text>
-                <MaterialIcons
-                    name="close"
-                    size={24}
-                    onPress={onClear}
-                    color={theme.colors.gray_400}
-                />
-            </View>
 
+export function Selected({ quantity, onClear, onSearch }: Props) {
+  return (
+    <Animated.View
+      entering={SlideInDown.duration(500)}
+      exiting={BounceOutDown}
+      style={styles.container}
+    >
+      <View style={styles.header}>
+        <Text style={styles.label}>{quantity} ingredientes selecionados</Text>
 
-        </Animated.View>
-    )
+        <MaterialIcons
+          name="close"
+          size={24}
+          color={theme.colors.gray_400}
+          onPress={onClear}
+        />
+      </View>
+
+      <Button title="Encontrar" onPress={onSearch} />
+    </Animated.View>
+  )
 }
