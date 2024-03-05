79176da8ac2a35f5831a9df10820478ef1c516d1
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-05 11:14:18 -0300

    Init - configuração inicial (stilos e fonts)

---
+++ src/app/index/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Index() {
+    return (
+        <View style={styles.container} >
+            <Text style={styles.title}>
+                Escolha os produtos
+            </Text>
+            <Text style={styles.subtitle}>
+                Descubra receitas baseadas que você escolheu.
+            </Text>
+        </View>
+    )
+}
